a47f99703f4923ced1560a454328831c2f00dfb1
James <James@152.23.66.44>
2019-08-30 11:52:59 -0400

    Jedi completed

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,8 +7,48 @@ public class A1Jedi {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
 		// Your code follows here.
-		
+		//Initialize arrays for items and their price
+		int numItem = scan.nextInt();
+		String[] item = new String[numItem];
+		int[] itemBought = new int[numItem];
+		int[] cus = new int[numItem];
+		for(int i = 0; i < numItem; i ++)
+		{
+			item[i] = scan.next();
+			scan.nextDouble();
+		}		
+		int numCus = scan.nextInt();
+		for(int i = numCus; i > 0; i --)
+		{
+			while(!scan.hasNextDouble())
+			{
+				scan.next();
+			}
+			int t = scan.nextInt();//number of items to buy
+			for(int x = 0; x < t; x++)
+			{
+				int n = scan.nextInt();
+				String thing = scan.next();
+				//search for "thing" in arrays
+				for(int index = 0; index < numItem; index ++)
+				{
+					if(item[index].equals(thing))
+					{
+						itemBought[index] += n;
+						cus[index] ++;
+					}
+				}
+			}
+		}
+		for(int i = 0; i < numItem; i ++)
+		{
+			if(cus[i] == 0)
+			{
+				System.out.println("No customers bought " + item[i]);
+			}
+			else 
+				System.out.println(cus[i] + " customers bought " + itemBought[i] + " " + item[i]);
+		}
 	}
 }
